b1630c1b1c467591169471b4e78d0d533812a55d
Vince <vinceevangelista028@gmail.com>
2025-10-17 04:06:03 +0800

    add: complete skill and experience analyzer

--- src/main/java/ExperienceAnalyzer.java
+++ src/main/java/ExperienceAnalyzer.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 
 public class ExperienceAnalyzer implements Analyzers{
@@ -13,10 +14,14 @@ public class ExperienceAnalyzer implements Analyzers{
     @Override
     public double dateRelevance(LocalDate startDate, LocalDate endDate) {
 
+        double score = 0;
+
         // Step 1: Receive input
         // - startDate
         // - endDate
         // - longer the better
+
+        long totalDays =  ChronoUnit.DAYS.between(startDate, endDate);
 
         // Step 3: Calculate actual duration
         // actualDurationDays = days between startDate and endDate
@@ -26,45 +31,89 @@ public class ExperienceAnalyzer implements Analyzers{
         // - +10 10 years
         // - +99999999999 95 years
 
+        if(totalDays/365 <= 1 && totalDays/365 > 0){
+            score += 5;
+        }else if(totalDays/365 == 10){
+            score += 10;
+        }else{
+            score += 999;
+        }
 
         // Step 6: Return the points
 
-        return 0;
+        return score;
     }
 
     @Override
     public double credentialRelevance(String credential1, String credential2) {
+        double score = 0;
 
         // Step 1: Receive input
         // credential1: the professional position (e.g., "Manager", "Engineer", "Intern")
         // credential2: the name or tier of the organization (e.g., "Google", "Startup", "Local Business")
 
+         for (int i = 0; i < job.experiencePositions.length; i++) {
+            if (credential1.equalsIgnoreCase(job.experiencePositions[i])) {
+                score += job.positionPoints[i];
+                break;
+            }
+        }
         // Step 2: Match input position with job.experiencePositions[] and assign job.positionPoints[] value
+
+        for (int i = 0; i < job.experienceCompanies.length; i++) {
+            if (credential2.equalsIgnoreCase(job.experienceCompanies[i])) {
+                score += job.companyPoints[i];
+                break;
+            }
+        }
         // Step 3: Match input company with job.experienceCompanies[] and assign job.companyPoints[] value
-        // Step 4: Match input achievement with job.experienceAchievements[] and assign job.experienceAchievementPoints[] value
         // Step 5: Sum all assigned values for total experience score
-
-
-        return 0;
+        return score;
     }
 
     @Override
     public double achievementRelevance(ArrayList<String> achievements) {
 
+        double score = 0;
         // Step 1: Take ArrayList<String> of input achievements
+
+        for(int i = 0; i < achievements.size(); i++){
+            for(int j = 0; j< job.experienceAchievements.length; j++){
+                if(job.experienceAchievements[j].equalsIgnoreCase(achievements.get(i))){
+                    score += job.experienceAchievementPoints[j];
+                    break;
+                }
+            }
+        }
         // Step 2: Compare each entry with job.experienceAchievements[]
         // Step 3: For every match, add corresponding job.experienceAchievementPoints[] value
         // Step 4: Return the total points
 
 
         // return points
-        return 0;
+        return score;
     }
 
     @Override
     public double analyze(ResumeSection resumeSection) {
 
-        Util.printBanner("Experience: " + 0);
-        return 0;
+        Experience experienceResume = (Experience) resumeSection;
+
+        LocalDate startDate = experienceResume.getStartDate();
+        LocalDate endDate = experienceResume.getEndDate();
+
+        String credential1 = experienceResume.getRole();
+        String credential2 = experienceResume.getCompany();
+
+        ArrayList<String> achievements = experienceResume.getAchievements();
+
+        double score = 0;
+
+        score += dateRelevance(startDate, endDate);
+        score += credentialRelevance(credential1, credential2);
+        score += achievementRelevance(achievements);
+
+        Util.printBanner("Experience: " + score);
+        return score;
     }
 }
--- src/main/java/SkillAnalyzer.java
+++ src/main/java/SkillAnalyzer.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 
 public class SkillAnalyzer implements Analyzers{
@@ -12,54 +13,106 @@ public class SkillAnalyzer implements Analyzers{
     @Override
     public double dateRelevance(LocalDate startDate, LocalDate endDate) {
 
+
+        double score = 0;
         // Step 1: Receive input
         // - startDate: when the started using skills
         // - endDate: current date today
 
-
+        long totalDays =  ChronoUnit.DAYS.between(startDate, endDate);
         // Step 2: Calculate points longer = better
+
+
+        if(totalDays/365 <= 1 && totalDays/365 > 0){
+            score += 5;
+        }else if(totalDays/365 <= 5){
+            score += 10;
+        }else{
+            score += 999;
+        }
 
         // Step 4: Assign points based on timeliness
         // - +10 points = 5 years etc
 
 
         // Step 6: Return the points
-        return 0;
+        return score;
     }
 
     @Override
     public double credentialRelevance(String credential1, String credential2) {
 
+        double score = 0;
         // Step 1: Receive input
         // - proficiency: level of expertise (e.g., "Expert", "Intermediate", "Beginner")
         // - skill: type of skill or technology (e.g., "Java", "Python", "Excel")
 
+        for (int i = 0; i < job.skillProficiencies.length; i++) {
+            if (credential1.equalsIgnoreCase(job.skillProficiencies[i])) {
+                score += job.proficiencyPoints[i];
+                break;
+            }
+        }
+
         // Step 2: Match input proficiency with job.skillProficiencies[] and assign job.proficiencyPoints[] value
+
+        for (int i = 0; i < job.skillTechnologies.length; i++) {
+            if (credential2.equalsIgnoreCase(job.skillTechnologies[i])) {
+                score += job.technologyPoints[i];
+                break;
+            }
+        }
         // Step 3: Match input technology with job.skillTechnologies[] and assign job.technologyPoints[] value
         // Step 4: Sum both for total skill score
 
 
         // return points
 
-        return 0;
+        return score;
     }
 
     @Override
     public double achievementRelevance(ArrayList<String> achievements) {
 
+        double score = 0;
         // Step 1: Take ArrayList<String> of input achievements
+
+        for(int i = 0; i < achievements.size(); i++){
+            for(int j = 0; j< job.experienceAchievements.length; j++){
+                if(job.skillAchievements[j].equalsIgnoreCase(achievements.get(i))){
+                    score += job.skillAchievementPoints[j];
+                    break;
+                }
+            }
+        }
         // Step 2: Compare each entry with job.skillAchievements[]
         // Step 3: For every match, add corresponding job.skillAchievementPoints[] value
         // Step 4: Return the total points
 
-        return 0;
+        return score;
     }
 
     @Override
     public double analyze(ResumeSection resumeSection) {
 
-        Util.printBanner("Skill: " + 0);
-        return 0;
+        Skill skillResume = (Skill) resumeSection;
+
+        LocalDate startDate = skillResume.getStartDate();
+        LocalDate endDate = skillResume.getEndDate();
+
+        String credential1 = skillResume.getProficiency();
+        String credential2 = skillResume.getSkill();
+
+        ArrayList<String> achievements = skillResume.getAchievements();
+
+        double score = 0;
+
+        score += dateRelevance(startDate, endDate);
+        score += credentialRelevance(credential1, credential2);
+        score += achievementRelevance(achievements);
+
+        Util.printBanner("Skill: " + score);
+        return score;
     }
     // Analyze the entire resume give a score and grade it Needs improvement, Average, Strong, Excellent
 }
